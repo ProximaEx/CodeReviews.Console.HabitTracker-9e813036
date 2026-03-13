a54a4d638f243d7780de5a8bd4d09302ad2bf1ca
ProximaEx <151698396+ProximaEx@users.noreply.github.com>
2026-03-12 22:58:35 -0400

    Added window formatting, center text methods. Improved Title screen and animation. Animation method available for more future visuals. Some more validation and improved menu flow. Started functionality for adding and viewing different habit types. Added non-joined table for storing habit types that updates a list of local habit types when opening app.

--- habitTracker.ProximaEx/HabitTrackerProgram.cs
+++ habitTracker.ProximaEx/HabitTrackerProgram.cs
@@ -2,6 +2,8 @@
 using Microsoft.Data.Sqlite;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
 
 
 /* ----- Project Reqs -----
@@ -44,90 +46,187 @@ namespace habitTracker.ProximaEx;
 
 class HabitTrackerProgram
 {
-	const string connectionString = "Data Source=coffee-tracker.db";
+	const string connectionString = "Data Source=habit-log.db";
 	static readonly SqliteConnection connection = new(connectionString);
+	static int width;
+	static int height;
+	static List<string> habitTypes = [ "Coffee" ];
+	static int currentHabitIndex = 1;
+	static bool SelectAllAdded = false;
+	//Add 'all' functionality for viewing all habits in db
+	//list to store habit types
+	//after creating table, call sql query to add any other habit types
 
 	static void Main(string[] args)
 	{
-		using (connection)
-		{
-			connection.Open();
-			var tableCmd = connection.CreateCommand();
-			tableCmd.CommandText =
-				@"CREATE TABLE IF NOT EXISTS habit_table (
-				Id INTEGER PRIMARY KEY AUTOINCREMENT,
-				Habit TEXT,
-				Date TEXT,
-				Quantity INTEGER
-				)";
-			tableCmd.ExecuteNonQuery();
-			connection.Close();
-		}
-		LoadingScreen();
+		SetWindow();
+		CreateTablesIfNotExists();
+		GetHabitTypes();
+		//FlashTitle();
+		//CoffeeAnimation();
 		Menu();
 	}
-
-	static void LoadingScreen()
+	static void FlashTitle()
 	{
+		string[] titleScreen =
+		{
+			"                 ___   ___               ",
+			"   ____   ____  ||``\\ ||``\\  ___   ___   ",
+			"  ||``\\| ||``|| ||__  ||__  ||`|| ||`||  ",
+			"  ||     ||  || |!``  |!``  |!==' |!=='  ",
+			"  ||__/| ||__|| ||    ||    ||__. ||__.  ",
+			"   ````   ````  ``    ``     ````  ````  ",
+			"",
+			"",
+			"  .                        ",
+			" /  __.  __~  __~  __,  ,_ ",
+			"/  /__/ /__/ /__/ /--` /  `",
+			"        ,_/  ,_/  `-       "
+		};
+		int topY = height / 2 - titleScreen.Length / 2;
+		Console.SetCursorPosition(0, topY);
+		foreach (string line in titleScreen) { Console.WriteLine(CenterText(line)); }
+		Thread.Sleep(1200);
+
+		string[,] notJustCoffee = { { CenterText("=========================================") } };
+		Animate(notJustCoffee, [0, topY + 3], 500);
+		string[,] message = { { CenterText("h a b i t") } };
+		Animate(message, [0, topY + 6], 1500);
+
 		Console.Clear();
-		Console.WriteLine("Coffee Tracker (TM)");
-		Console.WriteLine("Loading...");
-		//var loading = new Thread();
-		//loading.Start();
-		Thread.Sleep(1000);
+	}
+
+	static void CoffeeAnimation()
+	{	
+		string[,] loadCells =
+		{
+			{
+			"        \\ .         ",
+			"       ' ) )        ",
+			"        / / '       ",
+			"    ._________.     ",
+			"    ||       ==v\\\\,  ",
+			"    ||        |  || ",
+			"    ||        |  || ",
+			"    ||       ==*/`  ",
+			"     \\\\_____./      ",
+			},
+			{
+			"       ' ) ;        ",
+			"        / / '       ",
+			"       (  \\         ",
+			"    ._________.     ",
+			"    ||       ==v\\\\,  ",
+			"    ||        |  || ",
+			"    ||        |  || ",
+			"    ||       ==*/`  ",
+			"     \\\\_____./      ",
+			},
+			{
+			"        ./ `        ",
+			"       ( (.         ",
+			"       ,\\  \\        ",
+			"    ._________.     ",
+			"    ||       ==v\\\\,  ",
+			"    ||        |  || ",
+			"    ||        |  || ",
+			"    ||       ==*/`  ",
+			"     \\\\_____./      ",
+			},
+			{
+			"       c.(          ",
+			"        ;\\ \\        ",
+			"       ' ) .)       ",
+			"    ._________.     ",
+			"    ||       ==v\\\\,  ",
+			"    ||        |  || ",
+			"    ||        |  || ",
+			"    ||       ==*/`  ",
+			"     \\\\_____./      ",
+			},
+		};
+		int[] topLeftCoordsForCenteredAnimation =
+			[ width / 2 - loadCells[0,0].Length / 2 , height / 2 - loadCells.GetLength(1) / 2 ];
+		for (int i = 0; i < 3; i++) { Animate(loadCells, topLeftCoordsForCenteredAnimation, 250); }
 	}
 
 	static void Menu()
-	{
+	{	
+		if (!SelectAllAdded && habitTypes.Count >= 2) { habitTypes.Add("All"); SelectAllAdded = true; }
+		string[] menuStrings = [
+				"\n",
+				"Welcome to Coffee, um I mean Habit Logger",
+				"Track whatever...",
+				"\n",
+				"   +---------------+   ",
+				"<  |               |  >",
+				"   +---------------+   ",
+				"\n",
+				"Please select an option:",
+				"",
+				" 1 - Log habit occurance   ",
+				" 2 - View history          ",
+				" 3 - Edit a record         ",
+				" 4 - Delete a record       ",
+				" 5 - Add a new habit type  ",
+				"\n\n\n",
+				"Type 'exit' to leave."
+		];
 		bool exit = false;
 		while (!exit)
 		{
-			DrawMenu();
-			Console.SetCursorPosition(0, 14);
-			Console.Write("                                          \r");
-			string? menuInput = Console.ReadLine();
-
-			switch (menuInput)
+			Draw(menuStrings);
+			Console.SetCursorPosition(width / 2 - 7, 7);
+			Console.Write(CenterText(habitTypes[currentHabitIndex], 13));
+			Console.SetCursorPosition(width / 2 - 1, 19);
+			var keyInput = Console.ReadKey(false);
+			if (keyInput.Key == ConsoleKey.LeftArrow || keyInput.Key == ConsoleKey.RightArrow)
 			{
-				case "exit":
-					exit = true;
-					break;
-				case "1":
-					Insert();
-					break;
-				case "2":
-					ViewAll();
-					Console.WriteLine("Press any key to exit");
-					Console.ReadKey();
-					break;
-				case "3":
-					Edit();
-					break;
-				case "4":
-					Delete();
-					break;
-				default:
-					Console.WriteLine("Please enter a valid option.");
-					Thread.Sleep(1000);
-					break;
+				HabitSelector(keyInput);
+			}
+			else if (keyInput.Key == ConsoleKey.Backspace)
+			{
+				Console.Write("\b");
+			}
+			else
+			{
+				char charInput = keyInput.KeyChar;
+				string menuInput = char.ToString(charInput) + Console.ReadLine();
+
+				switch (menuInput)
+				{
+					case "exit":
+						exit = true;
+						break;
+					case "1":
+						Insert();
+						break;
+					case "2":
+						ViewAll();
+						Console.ReadKey();
+						break;
+					case "3":
+						Edit();
+						break;
+					case "4":
+						Delete();
+						break;
+					//case "5":
+					//	AddType();
+					//	break;
+					default:
+						Console.Write(CenterText("Please enter a valid option."));
+						Thread.Sleep(500);
+						break;
+				}
 			}
 		}
 	}
 
-	static void DrawMenu()
+	static void Draw(string[] cell)
 	{
 		Console.Clear();
-		string[] menuStrings = {
-				"\n",
-				"Welcome to Coffee Tracker",
-				"Drink more coffee\n\n",
-				"Please select an option:\n",
-				" 1 - Log coffee drinking",
-				" 2 - View coffee drinking",
-				" 3 - Edit a coffee record",
-				" 4 - Delete a coffee record\n\n\n\n\n\n",
-				"Type 'exit' to leave."};
-		foreach (string s in menuStrings) { Console.WriteLine(s); }
+		foreach (string s in cell) { Console.WriteLine(CenterText(s)); }
 	}
 
 	static void Insert()
@@ -141,7 +240,7 @@ class HabitTrackerProgram
 			connection.Open();
 			var tableCmd = connection.CreateCommand();
 			tableCmd.CommandText =
-				$@"INSERT INTO habit_table ( Date, Quantity ) VALUES ( '{date}', {quantity} )";
+				$@"INSERT INTO habit_table ( Date, Quantity, Habit ) VALUES ( '{date}', {quantity}, 'coffee')";
 			tableCmd.ExecuteNonQuery();
 			connection.Close();
 		}
@@ -157,26 +256,30 @@ class HabitTrackerProgram
 			connection.Open();
 			var tableCmd = connection.CreateCommand();
 			tableCmd.CommandText = 
-				@"SELECT * FROM habit_table";
+				@$"SELECT * FROM habit_table";
 			var reader = tableCmd.ExecuteReader();
 
 			if (reader.HasRows)
 			{
 				while (reader.Read())
 				{
-					queryRecords.Add( new CoffeeRecord(reader.GetInt32(0),reader.GetString(1),reader.GetInt32(2)) );
+					queryRecords.Add( new CoffeeRecord(reader.GetInt32(0),reader.GetString(1),reader.GetInt32(2),reader.GetString(3)));
 				}
-			} else { Console.WriteLine("No data found"); }
+			} else { Console.WriteLine(CenterText("No data found\n")); }
 
 			reader.Close();
 			connection.Close();
 		}
-		Console.WriteLine("\n------------- Records -------------");
-		foreach (CoffeeRecord row in queryRecords)
+		if (queryRecords.Count > 0)
 		{
-			Console.WriteLine($"Id: {row.Id}\t{row.Date}\tQuantity: {row.Quantity}");
+			Console.WriteLine(CenterText("------------- Records -------------"));
+			foreach (CoffeeRecord row in queryRecords)
+			{
+				Console.WriteLine(CenterText($"Id: {row.Id}\t  {row.Date}\tQuantity: {row.Quantity}"));
+			}
+			Console.WriteLine(CenterText("-----------------------------------\n"));
 		}
-		Console.WriteLine("-----------------------------------\n");
+		Console.WriteLine(CenterText("Press any key to exit"));
 	}
 
 	static void Edit()
@@ -229,33 +332,159 @@ class HabitTrackerProgram
 		}
 	}
 
-	public static int GetNumInput(string message)
+	static int GetNumInput(string message)
 	{
 		while (true)
 		{
 			Console.WriteLine(message);
 			string? input = Console.ReadLine();
+			if (input == "exit") { Menu(); }
 			if (int.TryParse(input, out int result) && result > 0 && result < 100) { return result; }
 			else { Console.WriteLine("Invalid quantity"); }
 		}
 	}
 
-	public static string GetDateInput()
+	static string GetDateInput()
 	{
 		while (true)
 		{
 			Console.WriteLine("Enter date (formatted mm-dd-yy)");
 			string? input = Console.ReadLine();
+			if (input == "exit") { Menu(); }
 			if (DateTime.TryParseExact(input, "MM-dd-yy", new CultureInfo("en-US"), 0, out DateTime outyDate)) { return input; }
 			else { Console.WriteLine("Invalid date"); }
 		}
 	}
+
+	static void Animate(string[,] cells, int[] topLeftCoords, int threadSleep)
+	{
+		int[] bottomRightCoords = { topLeftCoords[0] + cells[0,0].Length - 1, topLeftCoords[1] + cells.GetLength(1) - 1 };
+		for (int i = 0; i < cells.GetLength(0); i++)
+		{
+			ClearRange(topLeftCoords, bottomRightCoords);
+			for (int j = 0; j < cells.GetLength(1); j++)
+			{
+				Console.SetCursorPosition(topLeftCoords[0], topLeftCoords[1] + j);
+				Console.Write(cells[i, j]);
+			}
+			Thread.Sleep(threadSleep);
+		}
+	}
+
+	static void ClearRange(int[] topLeftCoords, int[] bottomRightCoords)
+	{
+		int rowQuantity = bottomRightCoords[1] - topLeftCoords[1] + 1;
+		int clearLength = bottomRightCoords[0] - topLeftCoords[0] + 1;
+		for (int i = 0; i < rowQuantity; i++)
+		{
+			Console.SetCursorPosition(topLeftCoords[0], topLeftCoords[1] + i);
+			Console.Write("".PadLeft(clearLength));
+		}
+	}
+
+	static void SetWindow()
+	{
+		int targetW = 72;
+		int targetH = 24;
+		width = (targetW > Console.LargestWindowWidth) ? Console.LargestWindowWidth : targetW;
+		height = (targetH > Console.LargestWindowHeight) ? Console.LargestWindowHeight : targetH;
+		if (OperatingSystem.IsWindows())
+		{
+			Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+			Console.SetWindowSize(width, height);
+			Console.SetBufferSize(width, height);
+		}
+		else
+		{
+			width = Console.WindowWidth;
+			height = Console.WindowHeight;
+		}
+	}
+
+	static string CenterText(string input)
+	{
+		return input.PadLeft(width / 2 + input.Length / 2);
+	}
+
+	static string CenterText(string input, int boundWidth)
+	{
+		return input.PadLeft(boundWidth / 2 + input.Length / 2);
+	}
+
+	static void HabitSelector(ConsoleKeyInfo readKey)
+	{
+		if (readKey.Key == ConsoleKey.RightArrow)
+		{ currentHabitIndex = ((currentHabitIndex + habitTypes.Count + 1) % habitTypes.Count); }
+		if (readKey.Key == ConsoleKey.LeftArrow)
+		{ currentHabitIndex = ((currentHabitIndex + habitTypes.Count - 1) % habitTypes.Count); }
+	}
+
+	static void GetHabitTypes()
+	{
+		using (connection)
+		{
+			connection.Open();
+			var tableCmd = connection.CreateCommand();
+			tableCmd.CommandText =
+				@$"SELECT * FROM habit_types";
+			var reader = tableCmd.ExecuteReader();
+
+			if (reader.HasRows)
+			{
+				while (reader.Read())
+				{
+					habitTypes.Add( reader.GetString(0) );
+				}
+			}
+			reader.Close();
+			connection.Close();
+		}
+	}
+
+	static void CreateTablesIfNotExists()
+	{
+		string[] initialCmds =
+		[
+			@"CREATE TABLE IF NOT EXISTS habit_table (
+				Id INTEGER PRIMARY KEY AUTOINCREMENT,
+				Date TEXT,
+				Quantity INTEGER,
+				Habit TEXT
+				)",
+			@"CREATE TABLE IF NOT EXISTS habit_types (
+				Type TEXT PRIMARY KEY
+				)"
+		];
+		using (connection)
+		{
+			connection.Open();
+			foreach (string cmd in initialCmds)
+			{
+				var tableCmd = connection.CreateCommand();
+				tableCmd.CommandText = cmd;
+				tableCmd.ExecuteNonQuery();
+			}
+			connection.Close();
+		}
+	}
+
+	static void AddType()
+	{
+		//return current types from db
+		//prompt for new
+		//validate for string length
+		//validate for not existing
+		//add to db type table
+		//add to type list
+		//return to menu
+	}
 }
 
-public class CoffeeRecord(int idIn, string dateIn, int quantityIn)
+public class CoffeeRecord(int idIn, string dateIn, int quantityIn, string habit)
 {
 	public int Id { get; } = idIn;
 	public string Date { get; } = dateIn;
 	public int Quantity { get; } = quantityIn;
+	public string Habit { get; } = habit;
 }
 
